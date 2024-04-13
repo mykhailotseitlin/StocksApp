cb1fa849d47413b66e842fdd11ee8597f4fa86d1
Mykhailo Tseitlin <tseytlin21@gmail.com>
2024-04-13 10:58:27 +0300

    Initial commit

---
+++ StocksApp/StocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppApp.swift
+//  StocksApp
+//
+//  Created by Mykhailo Tseitlin on 13.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
